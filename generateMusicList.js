9037e3455c8ae5f2f2ab2e85706f4d85761541ef
aaberey <pik4chug4m3rbr@gmail.com>
2024-10-18 16:33:11 -0300

    bons tempos fix

--- generateMusicList.js
+++ generateMusicList.js
@@ -2,8 +2,8 @@ const fs = require('fs').promises; // Usando a versão de promessas do fs
 const path = require('path');
 const mm = require('music-metadata');
 
-var musicDir = './music/Bonda1'; // Substitua pelo caminho correto
-var coverImage = './images/Bonda1.png'; // Imagem da capa
+var musicDir = './music/BonsTempos'; // Substitua pelo caminho correto
+var coverImage = './images/BonsTempos.png'; // Imagem da capa
 
 
 
@@ -46,7 +46,7 @@ async function generateMusicList() {
     musicList.sort((a, b) => (a.numero || 0) - (b.numero || 0));
 
     console.log('Lista de músicas gerada:', musicList);
-    await fs.writeFile('Bonda1.json', JSON.stringify(musicList, null, 2));
+    await fs.writeFile('BonsTempos.json', JSON.stringify(musicList, null, 2));
     console.log('Arquivo musicas.json gerado com sucesso!');
 
   } catch (err) {
